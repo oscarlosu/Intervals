23ef3ed0e149ed1f83e6dc01531a7f60f5ff55f0
Oscar Losada <oscarlosu@gmail.com>
2016-03-26 13:34:41 +0100

    Created interval blocks. Created test level.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Interval {
+    min2,
+    Maj2,
+    min3,
+    Maj3,
+    P4,
+    A4,
+    P5,
+    min6,
+    Maj6,
+    min7,
+    Maj7,
+    P8
+}
+public class Block : MonoBehaviour {
+
+    public Interval Interval;
+
+
+
+}
